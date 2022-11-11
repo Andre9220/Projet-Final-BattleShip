3a1fc5afdf182af299b16d9ebce3efe3edc82ce1
YassineBoutouil <yassine.boutouil.pro@gmail.com>
2022-11-11 19:30:34 +0100

    Méthode qui lit le choix de l'utilisateur  return 4 si l'utilisateur rentre "Q" ou "q"

--- NavalBattle-1/src/view/NavalBattleView.java
+++ NavalBattle-1/src/view/NavalBattleView.java
@@ -44,6 +44,40 @@ public class NavalBattleView {
     }
 
     /**
+     * Méthode qui affiche le menu principal puis lit le choix de l'utilisateur
+     * @return le choix de l'utilisateur (4 si le choix = 'q' ou 'Q')
+     */
+    public int getMenuOption() {
+
+        String strChoice;
+        int choice = 0;
+
+        do {
+            showMenu();
+            strChoice = sc.nextLine();
+            if (strChoice.equalsIgnoreCase("q"))
+                choice = 4;
+            else {
+                try {
+                    choice = Integer.valueOf(strChoice);
+                } catch (Exception e) {
+                    showMessage("invalid choice! try again");
+                }
+
+            }
+        } while (choice < 1 || choice > 4);
+        return choice;
+    }
+
+    /**
+     * Méthode qui permet d'afficher la chaine passée en parametre
+     * @param message
+     */
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    /**
      * Méthode qui affiche les règles du jeu
      */
 
