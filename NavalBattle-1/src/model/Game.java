996ebc0d5e2ad6f655683c20a8f861b01b8e431a
YassineBoutouil <yassine.boutouil.pro@gmail.com>
2022-11-12 14:44:11 +0100

    Update de Game avec ajout d'un attribut, le joueur qui gagne
    Ajout du squelette des méthodes Save et Load et ajout de la méthode qui permet de déterminer si une partie est fini

--- NavalBattle-1/src/model/Game.java
+++ NavalBattle-1/src/model/Game.java
@@ -12,5 +12,31 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Game {
-    public Player human, computer;
+    public Player human, computer, winner;
+
+    /**
+     * Constructeur par défaut
+     */
+    public Game() {
+        this.human = new Human();
+        this.computer = new Computer();
+    }
+
+    /**
+     * détermine si l'une des flottes des deux joueurs est coulée
+     * @return vrai si l'une des flottes des deux joueurs est coulée, faux sinon
+     */
+    public boolean isOver() {
+        return (human.getFleetHealth()==0 || computer.getFleetHealth()==0);
+    }
+
+    //à faire le plus rapidement possible !!!
+    public void saveToFile(String filename) throws IOException {
+
+    }
+
+    public void loadFromFile(String filename) throws IOException {
+
+    }
+
 }
