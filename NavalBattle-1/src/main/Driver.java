3cf69d65d714c897b47d73032eef043cbe076c0d
YassineBoutouil <yassine.boutouil.pro@gmail.com>
2022-11-14 18:28:06 +0100

    Ajout du main

--- NavalBattle-1/src/main/Driver.java
+++ NavalBattle-1/src/main/Driver.java
@@ -1,7 +1,16 @@
 package main;
 
+import controller.NavalBattleController;
+
 public class Driver {
 
+    public static void main(String[] args) {
 
+        // créer un objet NavalBattleController
+        NavalBattleController battle = new NavalBattleController();
+        // lancer le jeu
+        battle.start();
+
+    }
 
 }
