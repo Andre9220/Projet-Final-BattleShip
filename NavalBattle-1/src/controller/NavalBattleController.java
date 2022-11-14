3940991e7356bea9bde7c559da5b887c4a442477
ThomasDorby <tomflo93420@gmail.com>
2022-11-14 17:16:19 +0100

    Update NavalBattleController.java

--- NavalBattle-1/src/controller/NavalBattleController.java
+++ NavalBattle-1/src/controller/NavalBattleController.java
@@ -235,4 +235,226 @@ public class NavalBattleController {
         }
         return true;
     }
+
+    /**
+     * M�thode qui permet d'ex�cuter l'action de d�placement
+     * la m�thode permet d'avoir les coordonn�es du navire concern� et la direction souhait�e
+     * (demander au joueur si c'est un humain, ou g�n�rer al�atoirement si le joueur = Computer)
+     * @param player : joueur qui veut d�placer un navire
+     * @return vrai si le d�placement est effectu�, faux sinon
+     */
+    public boolean move(int player) {
+        Coordinates currCoor;
+        Ship ship = null;
+        boolean moved = false;
+        if (player == 0) {		// si le joueur = human
+            do {
+                int x = battleView.getRow("ship"); // avoir la ligne du navire
+                if (x == -1) {		// quitter si le joueur tape "q"
+                    quit = true;
+                    break;
+                }
+                int y = battleView.getCol("ship");	// avoir la colonne du navire
+                if (y == -1) {
+                    quit = true;
+                    break;
+                }
+                // l'utilisateur voit 1 .. 15 mais le programme traite 0 .. 14, c'est pour �a
+                // qu'on doit soustraire 1 pour les valeurs de coordonn�es saisies par l'utilisateur
+                currCoor = new Coordinates(x-1, y-1);
+
+                // avoir le navire � laquelle appartient la case choisie
+                ship = game.human.getShipFromCoor(currCoor);
+
+                // si pas de navire dans la case choisie, afficher un message
+                if (ship == null)
+                    battleView.showMessage("Human : There is no ship at this position! Try again");
+
+                    // si le navire choisi est touch� (ne doit pas etre d�plac�)
+                else if (ship.wasHit())
+                    battleView.showMessage("Human : Ship is hit, cant be moved! Try again");
+
+                // r�p�ter tant que la case choisie ne correspond pas � un navire qui peut etre d�plac�
+            } while (ship == null || ship.wasHit());
+
+        } else {	// si c'est l'ordinateur
+            // r�p�ter de g�n�rer des coordonn�es tant que ces derni�res
+            // ne correspondent pas � un navire qui peut etre d�plac�
+            do {
+                currCoor = new Coordinates((new Random()).nextInt(Constantes.GRID_ROWS), (new Random()).nextInt(Constantes.GRID_COLS));
+                ship = game.computer.getShipFromCoor(currCoor);
+
+            } while (ship == null || ship.wasHit());
+        }
+        // si le jouer n'as pas interrompu le jeu
+        if (!quit) {
+            int moveBy;
+            if (player == 0) {
+                // avoir la direction de d�placement correspond au choix du joueur humain
+                moveBy = battleView.getMoveBy(ship.isVertical());
+                if (moveBy == 0) 	// si le joueur tape "q"
+                    quit = true;
+                else {  // sinon tenter de d�placer le navire
+                    moved = moveShip(game.human.getOwnGrid(), ship, moveBy);
+                    if (moved)
+                        battleView.showMessage("Human : Ship moved.");
+                }
+            } else {
+                // g�n�rer une direction al�atoirement pour le joueur Computer
+                moveBy = (new Random()).nextInt(2) == 0 ? -1 : 1;
+                moved = moveShip(game.computer.getOwnGrid(), ship, moveBy);	// tenter le d�placement
+                if (moved)
+                    battleView.showMessage("Computer : Ship moved.");
+            }
+            if (!quit && moved)		// afficher les grilles apr�s le d�placement
+                battleView.showGrids(game, player, false, null);
+        }
+        return moved;
+    }
+
+
+    /**
+     * M�thode qui permet d'ex�cuter l'action de tir
+     * la m�thode permet d'avoir les coordonn�es du navire tireur et les coordonn�es de la cible
+     * (demander au joueur si c'est un humain, ou g�n�rer al�atoirement si le joueur = Computer)
+     * @param player : joueur qui veut d�placer un navire
+     */
+    public void fire(int player) {
+        flare = false;
+        Ship shooter = null;
+        Coordinates coor = null;
+        if (player == 0) {	// si le joueur = human
+            do {
+                int x = battleView.getRow("shooter ship");	// avoir la ligne du navire tireur
+                if (x == -1) {
+                    quit = true;
+                    break;
+                }
+                int y = battleView.getCol("shooter ship");	// avoir la colonne du navire tireur
+                if (y == -1) {
+                    quit = true;
+                    break;
+                }
+                coor = new Coordinates(x-1, y-1);
+                // avoir le navire concern� par les coordonn�es donn�es
+                shooter = game.human.getShipFromCoor(coor);
+
+                // si les coordonn�es donn�es ne correspondent � aucun navire, afficher un message
+                if (shooter == null)
+                    battleView.showMessage("Human : There is no ship at this position! Try again");
+
+                    // si les coordonn�es donn�es correspondent � un navire d�truit(ne peut pas tirer)
+                else if (shooter.getHealth()==0)
+                    battleView.showMessage("Human : This ship was totally destroyed! Try again");
+
+                // r�p�ter tant que les coordonn�es fournies ne correspondent pas
+                // � un navire qui peut tirer
+            } while (shooter == null || shooter.getHealth()==0);
+
+        } else {	// si c'est l'ordinateur
+            // r�p�ter de g�n�rer des coordonn�es tant que ces derni�res
+            // ne correspondent pas � un navire qui peut tirer
+            do {
+                coor = new Coordinates((new Random()).nextInt(Constantes.GRID_ROWS), (new Random()).nextInt(Constantes.GRID_COLS));
+                shooter = game.computer.getShipFromCoor(coor);
+
+            } while (shooter == null || shooter.getHealth()==0);
+        }
+        // si le jouer n'as pas interrompu le jeu
+        if (!quit) {
+            // voir si le navire tireur est un destroyer et si c'est son premir tir
+            flare = shooter instanceof Destroyer && ((Destroyer) shooter).canFlare();
+
+            if (player == 0) {
+                int x = battleView.getRow("target");	// avoir la ligne de la case cible
+                if (x == -1)
+                    quit = true;
+                else {
+                    int y = battleView.getCol("target");	// avoir la colonne de la case cible
+                    if (y == -1)
+                        quit = true;
+                    else
+                        targetCoor = new Coordinates(x-1, y-1);
+                }
+            } else {
+                // si c'est l'ordinateur, alors g�n�rer al�atoirement des coordonn�es de cible
+                targetCoor = new Coordinates((new Random()).nextInt(Constantes.GRID_ROWS), (new Random()).nextInt(Constantes.GRID_COLS));
+                // afficher l'action r�alis�e par le joueur computer
+                battleView.showCompAction(flare, targetCoor);
+            }
+            if (!quit) {
+                if (!flare) {	// si c'est pas une fus�e �clairante
+                    ArrayList<Coordinates> shootArea;
+                    // avoir la zone touch�e par le tir
+                    shootArea = getShootArea(shooter, targetCoor);
+                    Ship targetShip;
+                    if (player == 0) {
+                        // parcourir les cases touch�es par le tir
+                        for (int i=0; i<shootArea.size(); i++) {
+                            // avoir le navire concern� par chaque case touch�e
+                            targetShip = game.computer.getShipFromCoor(shootArea.get(i));
+                            // si la case touch�e en cours correspond � un navire
+                            if (targetShip != null) {
+                                // si la cible correspond � un sous-marin
+                                // et le tireur n'est pas un sous-marin, alors passer � la case suivante
+                                if (targetShip instanceof Submarine && !(shooter instanceof Submarine))
+                                    continue;
+                                // sinon
+                                // effectuer les modifications n�c�ssaires sur le navire touch�
+                                targetShip.destroyAt(shootArea.get(i));
+                                // effectuer les modifications sur la grille du joueur computer
+                                game.computer.destroyAt(shootArea.get(i));
+                                // effectuer les modifications sur la grille Ennemi du joueur human
+                                game.human.getEnemyGrid().destroy(shootArea.get(i));
+                                // si le tir a caus� la perte du joueur computer
+                                if (game.isOver()) {
+                                    game.winner = game.human;
+                                    battleView.showMessage("Human win!");
+                                    break;
+                                }
+
+                            }
+                        }
+
+                    } else {	// si c'est le joueur Computer
+                        // parcourir les cases touch�es par le tir
+                        for (int i=0; i<shootArea.size(); i++) {
+                            // avoir le navire concern� par chaque case touch�e
+                            targetShip = game.human.getShipFromCoor(shootArea.get(i));
+                            // si la case touch�e en cours correspond � un navire
+                            if (targetShip != null) {
+                                // si la cible correspond � un sous-marin
+                                // et le tireur n'est pas un sous-marin, alors passer � la case suivante
+                                if (targetShip instanceof Submarine && !(shooter instanceof Submarine))
+                                    continue;
+                                // sinon
+                                // effectuer les modifications n�c�ssaires sur le navire touch�
+                                targetShip.destroyAt(shootArea.get(i));
+                                // effectuer les modifications sur la grille du joueur human
+                                game.human.destroyAt(shootArea.get(i));
+                                // effectuer les modifications sur la grille Ennemi du joueur computer
+                                game.computer.getEnemyGrid().destroy(shootArea.get(i));
+                                // si le tir a caus� la perte du joueur human
+                                if (game.isOver()) {
+                                    game.winner = game.computer;
+                                    battleView.showMessage("Computer win!");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                } else {	// si c'est une fus�e �clairante
+                    // le Destroyer ne pourra plus tirer par la fus�e �clairante
+                    ((Destroyer) shooter).cantFlare();
+                }
+                // afficher les grilles en fonction des valeurs donn�es en param�tres
+                battleView.showGrids(game, player, flare, targetCoor);
+            }
+        }
+    }
+
+
+
+
+
 }
